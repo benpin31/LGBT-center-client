d5f37739425dc51e944468ff610eb8104c378273
ronronscelestes <julie.plantey@gmail.com>
2021-03-25 20:20:33 +0100

    apiHandler completed

--- src/apiHandler/apiHandler.js
+++ src/apiHandler/apiHandler.js
@@ -16,7 +16,7 @@ function errorHandler(error) {
 export default {
   service,
 
-  // AUTH 
+  // ---AUTH--- 
   signin(userInfo) {
     return service
       .post("/api/auth/signin", userInfo)
@@ -37,4 +37,113 @@ export default {
       .then((res) => res.data)
       .catch(errorHandler);
   },
+
+  // ---USER CRUD---
+  getUsers() {
+    return service
+    .get("/api/user")
+    .then((res) => res.data)
+    .catch(errorHandler);
+  },
+
+  getOneUser(itemId) {
+    return service
+    .get(`/api/user${itemId}`)
+    .then((res) => res.data)
+    .catch(errorHandler);
+  },
+
+  createUser(newUser) {
+    return service
+    .post("/api/user/create", newUser)
+    .then((res) => res.data)
+    .catch(errorHandler);
+  },
+
+  deleteUser(userId) {
+    return service
+    .delete(`/api/delete/${userId}`)
+    .then((res) => res.data)
+    .catch(errorHandler);
+  },
+
+  updateUser(userId, updatedInfo) {
+    return service
+    .patch(`/api/edit/${userId}`, updatedInfo)
+    .then((res) => res.data)
+    .catch(errorHandler);
+  },
+
+  // ---VISITS--- 
+  getVisits() {
+    return service
+    .get("/api/visits")
+    .then((res) => res.data)
+    .catch(errorHandler);
+  },
+
+  createVisit(visitInfo) {
+    return service
+    .post("/api/visits", visitInfo)
+    .then((res) => res.data)
+    .catch(errorHandler);
+  },
+
+  updateVisit(visitId, visitInfo) {
+    return service
+    .patch(`/api/visits/${visitId}`, visitInfo)
+    .then((res) => res.data)
+    .catch(errorHandler);
+  },
+
+  deleteVisit(visitId) {
+    return service
+    .delete(`/api/visits/${visitId}`)
+    .then((res) => res.data)
+    .catch(errorHandler);
+  },
+
+  // ---CATEGORIES--- 
+  getCategories() {
+    return service
+    .get("/api/categories")
+    .then((res) => res.data)
+    .catch(errorHandler);
+  },
+
+  createCategory(catefgoryInfo) {
+    return service
+    .post("/api/categories", catefgoryInfo)
+    .then((res) => res.data)
+    .catch(errorHandler);
+  },
+
+  updateCategory(categoryId, catefgoryInfo) {
+    return service
+    .patch(`/api/categories/${categoryId}`, catefgoryInfo)
+    .then((res) => res.data)
+    .catch(errorHandler);
+  },
+
+    // ---CONTACT TYPE--- 
+    getContactTypes() {
+      return service
+      .get("/api/contactTypes")
+      .then((res) => res.data)
+      .catch(errorHandler);
+    },
+  
+    createContactType(contactTypeInfo) {
+      return service
+      .post("/api/contactTypes", contactTypeInfo)
+      .then((res) => res.data)
+      .catch(errorHandler);
+    },
+  
+    updateContactType(contactTypeId, contactTypeInfo) {
+      return service
+      .patch(`/api/contactTypes/${contactTypeId}`, contactTypeInfo)
+      .then((res) => res.data)
+      .catch(errorHandler);
+    },
 };
