4ed49f2a2be0505a8e9647ae84ee2b3499ca1c47
Mathoz <mathieu.lambertin03@gmail.com>
2021-03-30 17:06:23 +0200

    graphic hot day

--- src/Views/Insights/HotDay.jsx
+++ src/Views/Insights/HotDay.jsx
@@ -1,12 +1,8 @@
 import React, { Component } from "react";
-import {
-  LineChart,
-  Line,
-  XAxis,
-  Tooltip,
-} from "recharts";
+import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
 import Calendar from "react-calendar";
 import apiHandler from "../../apiHandler/apiHandler";
+import Header from "../../Components/DataVizHeader";
 
 const CustomizedDot = (props) => {
   const { cx, cy, value } = props;
@@ -25,85 +21,109 @@ const CustomizedDot = (props) => {
 };
 
 export class HotDay extends Component {
-
   state = {
     data: null,
-    date: [new Date(new Date() - 31*24*3600*1000), new Date()],
+    date: [new Date(new Date() - 31 * 24 * 3600 * 1000), new Date()],
     //  default view : 1 month of data
+    calendarClicked: false,
   };
 
   setStartDate = (date) => {
     this.setState({ date }, () =>
       apiHandler
         .repartitionByWeeks({ dates: date })
-        .then((res) => this.setState({ data: res.agregatedData, date: res.updatedDates.map(date => new Date(date))  }))
+        .then((res) =>
+          this.setState({
+            data: res.agregatedData,
+            date: res.updatedDates.map((date) => new Date(date)),
+          })
+        )
         .catch((err) => console.log(err))
     );
+    this.setState({ calendarClicked: false });
   };
 
   componentDidMount() {
-    // const dateBegin = 
-    //   this.state.date[0].toISOString().substring(0, 10) + " 00:00:00";
-    // const dateEnd =
-    //   this.state.date[1].toISOString().substring(0, 10) + " 23:59:59";
+    const { date } = this.state;
 
-    const {date} = this.state ;
-
-    const dateBegin = new Date(Date.UTC(date[0].getFullYear(),date[0].getMonth(),date[0].getDate())) ;
-    const dateEnd = new Date(Date.UTC(date[1].getFullYear(),date[1].getMonth(),date[1].getDate(),23,59,59)) ;
+    const dateBegin = new Date(
+      Date.UTC(date[0].getFullYear(), date[0].getMonth(), date[0].getDate())
+    );
+    const dateEnd = new Date(
+      Date.UTC(
+        date[1].getFullYear(),
+        date[1].getMonth(),
+        date[1].getDate(),
+        23,
+        59,
+        59
+      )
+    );
 
     apiHandler
       .repartitionByWeeks({ dates: [dateBegin, dateEnd] })
       .then((res) => {
         console.log(res);
-        this.setState({ data: res.agregatedData, date: res.updatedDates.map(date => new Date(date)) });
+        this.setState({
+          data: res.agregatedData,
+          date: res.updatedDates.map((date) => new Date(date)),
+        });
       })
       .catch((err) => console.log(err));
   }
 
+  openCalendar = () => {
+    this.setState({ calendarClicked: !this.state.calendarClicked });
+  };
+
   render() {
-
+    const { date, data, calendarClicked } = this.state;
     return (
-      <div>
-        <div>
-          <h2>rapport</h2>
-          <div>
-            <p>Choisir dates</p>
-            <Calendar
-              onChange={this.setStartDate}
-              selectRange={true}
-              value={this.state.date}
-            />
-          </div>
-        </div>
+      <div className="main-container">
+        <Header
+          setStartDate={this.setStartDate}
+          calendarClicked={calendarClicked}
+          openCalendar={this.openCalendar}
+          date={date}
+        />
 
-        <div>
+        <div className="Graph-container">
           {this.state.data && (
-            <LineChart
-              width={500}
-              height={300}
-              data={this.state.data}
-              margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-              }}
-            >
-              {/* <CartesianGrid strokeDasharray="3 3" /> */}
-              <XAxis dataKey="name" />
-              {/* <YAxis /> */}
-              <Tooltip />
-              {/* <Legend /> */}
-              <Line
-                type="monotone"
-                dataKey="value"
-                stroke="#8884d8"
-                dot={<CustomizedDot />}
-                name="Visites"
-              />
-              {/* <Line type="monotone" dataKey="uv" stroke="#82ca9d" /> */}
-            </LineChart>
+            <ResponsiveContainer width="100%" height="100%">
+              <LineChart
+                width="100%"
+                height="100%"
+                data={this.state.data}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                {/* <CartesianGrid strokeDasharray="3 3" /> */}
+                <XAxis 
+                dataKey="name" 
+                tick={{stroke: "red"}}
+                tickLine={false}
+                />
+                <YAxis
+                axisLine={true}
+                tickLine={false}
+                />
+                <Tooltip cursor={false} />
+                {/* <Legend /> */}
+                <Line
+                  type="monotone"
+                  dataKey="value"
+                  stroke="#8884d8"
+                  dot={<CustomizedDot />}
+                  name="Visites"
+                  activeDot={false}
+                />
+                {/* <Line type="monotone" dataKey="uv" stroke="#82ca9d" /> */}
+              </LineChart>
+            </ResponsiveContainer>
           )}
         </div>
       </div>
