bf1358e78b908e51ad815381804c53b3fe522d1d
Mathoz <mathieu.lambertin03@gmail.com>
2021-03-31 15:27:11 +0200

    trying to play the laugh

--- src/Views/NotFound.jsx
+++ src/Views/NotFound.jsx
@@ -10,9 +10,7 @@ export class NotFound extends Component {
           src="https://media.tenor.co/images/68a83f039448147d2d56c179e43bb474/raw"
           alt="RuPaul GIF"
         />
-        <audio controls src="../Assets/Rupaul-Laughing.mp3">
-        </audio>{" "}
-      </div>
+    </div>
     );
   }
 }
